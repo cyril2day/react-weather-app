8d7c15421bae3465c4973532af3d5c9ecebd4266
psi <cy.lonsido@gmail.com>
2026-09-13 22:06:56 +0800

    Work on the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
 const Header = () => {
   return (
-    <div>
+    <div className='text-center mb-8'>
+      <h1 className='text-4xl font-bold text-white mb-2'>
+        Weather App
+      </h1>
+
+      <p className='text-blue-100'>
+        Stay updated with current weather conditions
+      </p>
     </div>
   )
 }
