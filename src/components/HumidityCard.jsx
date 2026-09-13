ac683aefe467d6f1c85ec078315708fcf0fbfe26
psi <cy.lonsido@gmail.com>
2026-09-13 23:22:27 +0800

    Build the HumidityCard component

--- src/components/HumidityCard.jsx
+++ src/components/HumidityCard.jsx
@@ -1,6 +1,15 @@
 const HumidityCard = ({ humidity }) => {
   return (
-    <div>
+    <div className='bg-white/10 rounded-xl p-4 mb-3'>
+      <div className='flex items-center space-x-2 mb-2'>
+        <span className='text-xl'>💧</span>
+
+        <span className='text-blue-100 text-sm'>Humidity</span>
+      </div>
+
+      <div className='text-white font-bold text-lg'>
+        {humidity}%
+      </div>
     </div>
   )
 }
